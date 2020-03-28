e0a042c3c9c9df0d8b7e9ce470a4284ef7ef6954
Alexis Tacnet <alexistacnet@gmail.com>
2020-03-28 17:50:11 +0100

    Update interface

--- interface.py
+++ interface.py
@@ -2,47 +2,61 @@ import time
 
 from nltk.stem import WordNetLemmatizer
 import click
+import pyfiglet
 
 from search_engine import SearchEngine
 
 
 @click.command()
-@click.option(
-    "--query", required=True, prompt="Search query", help="Your search query", type=str
-)
 @click.option("--count", default=10, help="Number of results.", type=int)
-def search(query, count):
-    """
-    This function is the main interface for querying the search engine.
-    """
-    start_time = time.time()
-
+def interface(count):
+    click.clear()
     click.secho("Loading search engine ...", fg="blue", bold=True)
     word_net_lemmatizer = WordNetLemmatizer()
     search_engine = SearchEngine(
         collection_name="cs276", stopwords_list=[], lemmatizer=word_net_lemmatizer,
     )
+    click.clear()
 
-    click.secho("Searching query ...", fg="blue", bold=True)
-    doc_scores_query = search_engine.search(query)
+    def search(query):
+        """
+        This function is the main interface for querying the search engine.
+        """
+        start_time = time.time()
+        click.secho("Searching query ...", fg="blue", bold=True)
+        doc_scores_query = search_engine.search(query)
 
-    click.secho("Sorting results ...", fg="blue", bold=True)
-    sorted_docs = [
-        k
-        for k, v in sorted(
-            doc_scores_query.items(), key=lambda item: item[1], reverse=True
+        click.secho("Sorting results ...", fg="blue", bold=True)
+        sorted_docs = [
+            k
+            for k, v in sorted(
+                doc_scores_query.items(), key=lambda item: item[1], reverse=True
+            )
+        ]
+
+        finished_time = time.time()
+        total_time = round(finished_time - start_time, 2)
+        click.secho(
+            "Finished ! Total time: {}s".format(total_time), fg="green", bold=True
         )
-    ]
 
-    finished_time = time.time()
-    total_time = round(finished_time - start_time, 2)
-    click.secho("Finished ! Total time: {}s".format(total_time), fg="green", bold=True)
+        for i, doc_id_query in enumerate(sorted_docs[:count]):
+            document = search_engine.collection.documents[doc_id_query]
+            click.secho(
+                "{}.\t{}/{}".format(i, document.folder, document.url), bold=True
+            )
+            click.secho("\t{}\n".format(" ".join(document.key_words)), fg="red")
 
-    for i, doc_id_query in enumerate(sorted_docs[:count]):
-        document = search_engine.collection.documents[doc_id_query]
-        click.secho("{}.\t{}/{}".format(i, document.folder, document.url), bold=True)
-        click.secho("\t{}\n".format(" ".join(document.key_words)), fg="red")
+    while True:
+        result = pyfiglet.figlet_format("Google 1998", font="big")
+        click.secho(result, fg="red", bold=True)
+        query = click.prompt(
+            click.style("Please enter you query", fg="blue", bold=True), type=str
+        )
+        search(query)
+        click.confirm("Do you want to continue?", abort=True)
+        click.clear()
 
 
 if __name__ == "__main__":
-    search()
+    interface()
